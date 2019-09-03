895515b2885e395361d0707e168866568acf9af8
Harry Zheng (CSI Interfusion Inc) <v-juzhe@microsoft.com>
2019-09-03 17:41:41 +0800

    add test runner cases

---
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/MyNestedTest.java
@@ -0,0 +1,34 @@
+package com.example.project;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.*;
+
+class MyNestedTest {   
+    @Nested
+    class ATests {
+        @Test
+        public void test() {
+            assertEquals(true, false);
+        }
+
+    }
+
+    @Nested
+    class A {
+        @Test
+        @DisplayName("Example test for method A")
+        void sampleTestForMethodA() {
+            System.out.println("Example test for method A");
+        }
+
+        @Nested        
+        class WhenX {           
+            @Test
+            @DisplayName("Example test for method A when X is true")
+            void sampleTestForMethodAWhenX() {
+                System.out.println("Example test for method A when X is true");                
+            }
+        }
+    }
+}
